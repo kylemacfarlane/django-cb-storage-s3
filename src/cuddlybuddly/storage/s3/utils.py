26b9cd1759d0e59a48824d7e0ca48f7c0563cacc
Kyle MacFarlane <kyle@deletethetrees.com>
2009-10-24 21:20:22 +0100

    CloudFrontURLs is now a proper unicode object

--- src/cuddlybuddly/storage/s3/utils.py
+++ src/cuddlybuddly/storage/s3/utils.py
@@ -19,30 +19,22 @@ def create_signed_url(file, expires=60, secure=False):
     )
 
 
-class CloudFrontURLs(object):
-    def __init__(self, default, patterns={}, https=None):
-        self._default = default
-        self._patterns = []
+class CloudFrontURLs(unicode):
+    def __new__(cls, default, patterns={}, https=None):
+        obj = super(CloudFrontURLs, cls).__new__(cls, default)
+        obj._patterns = []
         for key, value in patterns.iteritems():
-            self._patterns.append((re.compile(key), value))
-        self._https = https
+            obj._patterns.append((re.compile(key), unicode(value)))
+        obj._https = https
+        return obj
 
     def match(self, name):
         for pattern in self._patterns:
             if pattern[0].match(name):
                 return pattern[1]
-        return self._default
-
-    def __getattribute__(self, name):
-        if name in ('_default', '_patterns', '_https', 'https', 'match',
-                    '__unicode__'):
-            return object.__getattribute__(self, name)
-        return getattr(unicode(self.__unicode__()), name)
+        return self
 
     def https(self):
         if self._https is not None:
             return unicode(self._https)
-        return self.__unicode__()
-
-    def __unicode__(self):
-        return unicode(self._default)
+        return self
